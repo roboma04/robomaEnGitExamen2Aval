976dfab93da6eb5dde3ddcd0f01f4f7c45d34b40
RosaBosch <roboma@floridauniversitaria.com>
2023-02-14 16:19:50 +0100

    Modificacion del script emenor

--- deco.sh
+++ deco.sh
@@ -3,3 +3,4 @@
 ciudad=$1
 
 awk -v ciudad="$ciudad" 'BEGIN{sum=0;count=0}{if ($1==ciudad){sum+=$4;count+=1}}END{media=sum/count; if (media>=400){print "NO ECO"} else {print "ECO"}}' consumos.txt
+ 
