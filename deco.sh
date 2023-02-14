0566c791ffc32ca7c85f1d86329ee52fc930666f
RosaBosch <roboma@floridauniversitaria.com>
2023-02-14 16:09:58 +0100

    Creacion del script deco

---
+++ deco.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ciudad=$1
+
+awk -v ciudad="$ciudad" 'BEGIN{sum=0;count=0}{if ($1==ciudad){sum+=$4;count+=1}}END{media=sum/count; if (media>=400){print "NO ECO"} else {print "ECO"}}' consumos.txt
